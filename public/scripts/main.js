0caca4873d4325ad956eca9620c410bd92f81d7c
Davy De Waele <ddewaele@gmail.com>
2015-12-22 07:36:01 +0100

    initial commit

---
+++ public/scripts/main.js
@@ -0,0 +1,11 @@
+ var HelloWorld = React.createClass({
+
+ 	render: function() {
+ 		return (
+ 			<h1>HelloWorld !</h1>
+ 		);
+ 	}
+ });
+
+
+ReactDOM.render(<HelloWorld />,document.getElementById('content'));
